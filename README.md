e8e85be52c8ce3ee6d52eadd2ad688d9faf7bf1d
Colin Sindle <csindle@gmail.com>
2019-11-08 21:34:11 +0200

    Retry.

---
+++ README.md
@@ -0,0 +1,78 @@
+## Pi Pulse Width Modulation - Arm & Calibrate your ESC
+
+This tutorial / example shows how to use a Raspberry Pi's GPIO pins to
+control an Electronic Speed Controller (ESC).   Specifically, the manufacturer 
+specific arming and calibration procedures (there are many other tutorials
+on controlling servos, etc. with `pigpio`, Pis, and Python). 
+
+### Project Goals:
+
+Collect the ESC manufacture specific procedures for:
+
+* arming
+* calibration (throttle)
+* modes switching (braking, response-rate, music-tunes (!), etc).
+
+
+### Currently supported ESCs:
+
+* Turnigy:
+  * Plush (6A, 10A, 12A, 18A, 25A, 30A, 40A, 60A, 80A)
+  * Basic (18A, 25A))
+
+
+
+![Example components](images/setup.jpg)
+
+
+![ESC 5 volt Vcc (red) is *not* ESC 5 volt Vcc (red) is *not* connected.  The white signal
+wire is here connected to GPIO4, or actual pin 7.](images/GPIO_pin4.jpg)
+
+
+
+
+---
+
+**Warning: Remove propellers, etc. before testing, and 
+don't chop off anything important.**
+
+---
+
+### Everybody loves a code example:
+
+Calibrating a Turnigy ESC for full scale throttle/controller throws:
+
+``` python
+self.pwm(width=self.MAX_WIDTH)
+input("Connect power and press Enter to continue...")
+self.pwm(width=self.MAX_WIDTH, snooze=2)   # Official docs: "about 2 seconds".
+self.pwm(width=self.MIN_WIDTH, snooze=4)   # Time enough for the cell count, etc. beeps to play.
+```
+
+### Requirements:
+
+* [pigpio](http://abyz.me.uk/rpi/pigpio/python.html)
+
+
+### Installation:
+
+```bash
+sudo apt install python3-pigpio
+# if you want it to start on boot.
+sudo systemctl enable pigpiod
+# in this directory.
+pipenv sync
+```
+
+### Usage:
+
+* Read the code, 
+* Disconnect propellers, etc.
+*     python3 esc_demo.py
+
+
+### Contributing:
+
+Please open and issue or create a merge request if you can contribute more 
+working devices or timings.  Thanks!
+
---
+++ esc_demo.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import time
+import pigpio
+
+
+class TurnigyESC:
+    """
+    Turnigy Electonic Speed controller.
+    Tested with:
+      Plush 30 A
+    """
+    ## YMMV
+    MIN_WIDTH = 650
+
+    # Note that some batteries, 12 volt PSUs, etc. might only be capable of far less than this (e.g. 1350)
+    # However, the controllers range should still be set to max for finest full-scale resolution.
+    MAX_WIDTH = 2400
+
+    def __init__(self, pin):
+        self.conn = pigpio.pi()
+        self.pin = pin
+
+    def pwm(self, width: int, snooze: int = 0) -> None:
+        """
+        Convenience wrapper around pigpio's set_servo_pulsewidth.
+        width: The servo pulsewidth in microseconds. 0 switches pulses off.
+        snooze: seconds before returning.
+        """
+        print("Setting pulse width to", width, "microseconds for", snooze, "seconds.")
+        self.conn.set_servo_pulsewidth(self.pin, width)
+        if snooze:
+            time.sleep(snooze)
+        return
+
+    def calibrate(self) -> None:
+        """
+        This trains the ESC on the full scale (max - min range) of the controller / pulse generator.
+        This only needs to be done when changing controllers, transmitters, etc. not upon every power-on.
+        NB: if already calibrated, full throttle will be applied (briefly)!  Disconnect propellers, etc.
+        """
+        print("Calibrating...")
+        self.pwm(width=self.MAX_WIDTH)
+        input("Connect power and press Enter to continue...")
+        self.pwm(width=self.MAX_WIDTH, snooze=2)   # Official docs: "about 2 seconds".
+        self.pwm(width=self.MIN_WIDTH, snooze=4)   # Time enough for the cell count, etc. beeps to play.
+        print("Finished calibration.")
+
+    def arm(self) -> None:
+        """
+        Arms the ESC. Required upon every power cycle.
+        """
+        print("Arming...")
+        self.pwm(width=self.MIN_WIDTH, snooze=4)  # Time enough for the cell count, etc. beeps to play.
+        input("Connect power and press Enter to continue...")
+
+    def halt(self) -> None:
+        """
+        Switch of the GPIO, and un-arm the ESC.
+        Ensure this runs, even on unclean shutdown.
+        """
+        print("Slowing...")
+        self.pwm(width=self.MIN_WIDTH, snooze=1)   # This 1 sec seems to *hasten* shutdown.
+        print("Halting...")
+        self.pwm(0)
+        print("Stopping...")
+        self.conn.stop()
+        print("Halted.")
+
+    def test(self) -> None:
+        """
+        Test with a triangularish wave.
+        Useful for determining the max and min PWM values.
+        """
+        max_width = self.MAX_WIDTH - 1330  # microseconds
+        min_width = self.MIN_WIDTH + 80    # microseconds
+        step = 9  # microseconds
+
+        snooze = 0.3  # seconds
+        for i in range(1):
+            print("Increasing...")
+            for width in range(min_width, max_width, step):
+                self.pwm(width=width, snooze=snooze)
+
+        time.sleep(1)  # Duration test.  Seems to last almost 60s @ 1350.
+
+        snooze = 0.1
+        print("Decreasing...")
+        for width in range(max_width, min_width, -step):
+            self.pwm(width=width, snooze=snooze)
+
+
+if __name__ == "__main__":
+
+    esc = TurnigyESC(pin=4)
+    try:
+        #esc.calibrate()  # Needed when changing "transmitter".
+        esc.arm()
+        esc.test()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        esc.halt()
